8b85541d31cf8f59c96762987263ffd26436b70c
Alessio Chen <alessiochen98@gmail.com>
2023-09-30 16:20:55 +0200

    fix: examples/hello-world

--- src/examples/src/hello-world/App/composition.js
+++ src/examples/src/hello-world/App/composition.js
@@ -3,9 +3,9 @@ import { ref } from 'vue'
 export default {
   setup() {
     // Un "ref" è una fonte di dati reattiva che memorizza un valore.
-    // Tecnicamente, non abbiamo bisogno di wrappare la stringa con ref()
+    // Tecnicamente, non abbiamo bisogno di racchiudere la stringa con ref()
     // per poterla visualizzare, ma vedremo nel prossimo esempio
-    // perché è necessario se si intende modificare
+    // perché è necessario se volessimo cambiarne
     // il valore.
     const message = ref('Ciao Mondo!')
 
